9d2b299ba42b3b8cea9e80aa67bdc039b7fef3b2
AWieclawski <a_wieclawski@poczta.pl>
2019-07-12 14:57:56 +0200

    Changed data source configuration

--- src/main/java/edu/awieclawski/quiz/AppWar.java
+++ src/main/java/edu/awieclawski/quiz/AppWar.java
@@ -3,6 +3,7 @@ package edu.awieclawski.quiz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Enables self executing war package 
 @SpringBootApplication
 public class AppWar {
 
