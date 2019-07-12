4f6407854a5c58cfd24d853c58469294a032d677
AWieclawski <a_wieclawski@poczta.pl>
2019-07-12 11:50:19 +0200

    Add Maven dependencies in pom.xml

---
+++ src/main/java/edu/awieclawski/quiz/AppWar.java
@@ -0,0 +1,13 @@
+package edu.awieclawski.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWar {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppWar.class, args);
+    }
+
+}
